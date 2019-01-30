dcaa4aac72f583a3d7efc3bf139c5bea308236d4
Jaap Stefels <jaap@wcw-stud-145-109-28-29.wireless.uva.nl>
2019-01-30 17:33:34 +0100

    new map 8 bits

--- slicer_map.py
+++ slicer_map.py
@@ -1,9 +1,9 @@
 import os, gdal
 
-in_path = '/Users/jaap/Desktop/data_nlr/'
+in_path = '/Users/jaap/Desktop/leren_beslissen/'
 input_filename = 'top10nl_wegvlak_amersfoort.tif'
 
-out_path = '/Users/jaap/Desktop/data_nlr/map_data_1024/'
+out_path = '/Users/jaap/Desktop/leren_beslissen/map_data_1024_tif/'
 output_filename = 'tile_'
 
 tile_size_x = 1024
@@ -18,6 +18,6 @@ counter = 0
 
 for i in range(0, xsize, tile_size_x):
     for j in range(0, ysize, tile_size_y):
-        com_string = "gdal_translate -of GTIFF -srcwin " + str(i)+ ", " + str(j) + ", " + str(tile_size_x) + ", " + str(tile_size_y) + " " + str(in_path) + str(input_filename) + " " + str(out_path) + str(output_filename) + "_" + str(counter) + ".png"
+        com_string = "gdal_translate -of GTIFF -srcwin " + str(i)+ ", " + str(j) + ", " + str(tile_size_x) + ", " + str(tile_size_y) + " " + str(in_path) + str(input_filename) + " " + str(out_path) + str(output_filename) + "_" + str(counter) + ".tif"
         os.system(com_string)
         counter += 1
