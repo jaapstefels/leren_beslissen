f35c41b1fc3a4f1f0197636a19221d841365781a
Jaap Stefels <jaap@wcw-stud-145-109-28-29.wireless.uva.nl>
2019-01-30 16:49:08 +0100

    1024 dataset

---
+++ slicer_map.py
@@ -0,0 +1,23 @@
+import os, gdal
+
+in_path = '/Users/jaap/Desktop/data_nlr/'
+input_filename = 'top10nl_wegvlak_amersfoort.tif'
+
+out_path = '/Users/jaap/Desktop/data_nlr/map_data_1024/'
+output_filename = 'tile_'
+
+tile_size_x = 1024
+tile_size_y = 1024
+
+ds = gdal.Open(in_path + input_filename)
+band = ds.GetRasterBand(1)
+xsize = band.XSize
+ysize = band.YSize
+
+counter = 0
+
+for i in range(0, xsize, tile_size_x):
+    for j in range(0, ysize, tile_size_y):
+        com_string = "gdal_translate -of GTIFF -srcwin " + str(i)+ ", " + str(j) + ", " + str(tile_size_x) + ", " + str(tile_size_y) + " " + str(in_path) + str(input_filename) + " " + str(out_path) + str(output_filename) + "_" + str(counter) + ".png"
+        os.system(com_string)
+        counter += 1
