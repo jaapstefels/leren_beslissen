06551b9838a46cedb2943e4c8b81ae4630db6b0a
Jaap Stefels <jaap@wcw-stud-145-109-49-138.wireless.uva.nl>
2019-01-31 16:03:56 +0100

    jaja

--- black_white.py
+++ black_white.py
@@ -1,17 +1,14 @@
 from PIL import Image
+import os
+import shutil
 
-
-for i in os.listdir("/sat_data"):
-    if i.endswith(".tif"):
+for i in os.listdir("/Users/jaap/Desktop/leren_beslissen/map_data_1024_png"):
+    if i.endswith(".png"):
         file_in = i
         # file_out = './amersfoort_3band.tif'
         print(file_in)
 
-def convert(i):
-    col = Image.open("map_data_1024_png/tile_0.png")
-    gray = col.convert('L')
-    bw = gray.point(lambda x: 255 if x > 0 and x < 255  else 0, '1')
-    bw.save("tile_0_bw.png")
-
-if __name__ == "__main__":
-    main()
+        col = Image.open(i)
+        gray = col.convert('L')
+        bw = gray.point(lambda x: 255 if x > 0 and x < 255  else 255, '1')
+        bw.save("bw_{}".format(i))
